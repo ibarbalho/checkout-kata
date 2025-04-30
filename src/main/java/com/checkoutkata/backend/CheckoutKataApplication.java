9fe1f09cfe4e6672be689e3a191cf0ae73002ef1
Ittalo Barbalho <ittalo.renan@gmail.com>
2025-04-30 15:35:37 +0200

    initial project setup

---
+++ src/main/java/com/checkoutkata/backend/CheckoutKataApplication.java
@@ -0,0 +1,13 @@
+package com.checkoutkata.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutKataApplication.class, args);
+	}
+
+}
