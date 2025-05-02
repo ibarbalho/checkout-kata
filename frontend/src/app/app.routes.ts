ebbd1850ba047341da6dd783b604eb761da14d33
Ittalo Barbalho <ittalo.renan@gmail.com>
2025-05-02 17:33:09 +0200

    implement routing to urls

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/productlist', pathMatch: 'full' },
+    { path: 'productlist', component: ProductListComponent },
+    { path: 'items', component: ProductListComponent },
+    { path: 'checkout', component: CartComponent }
+  ];
--- frontend/src/app/shared/navbar.component.ts
+++ frontend/src/app/shared/navbar.component.ts
@@ -3,11 +3,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatBadgeModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatBadgeModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
