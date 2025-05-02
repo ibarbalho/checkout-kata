544c8314764ac9d1732a7a40b21ecb5cb2aca2e6
Ittalo Barbalho <ittalo.renan@gmail.com>
2025-05-02 17:31:56 +0200

    improve product list component

--- frontend/src/app/product-list/product-list.component.ts
+++ frontend/src/app/product-list/product-list.component.ts
@@ -1,10 +1,21 @@
-import { Component, effect, OnInit } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { NgFor, CurrencyPipe } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
-import { ItemService } from '../services/item.service';
+import { Item, ItemService } from '../services/item.service';
+import { CartService } from '../services/cart.service';
+import { Offer, OfferService } from '../services/offer.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { Router, RouterModule } from '@angular/router';
+
+interface ItemWithQuantity extends Item {
+  quantity: number;
+  specialOffer?: string;
+}
 
 @Component({
   selector: 'app-product-list',
@@ -15,23 +26,80 @@ import { ItemService } from '../services/item.service';
     MatCardModule,
     MatButtonModule,
     MatIconModule,
-    MatDividerModule
+    MatDividerModule,
+    MatProgressSpinnerModule,
+    MatToolbar,
+    MatGridListModule, RouterModule
   ],
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent {
 
-  constructor(private itemService: ItemService){
+  readonly items = signal<ItemWithQuantity[]>([]);
+  readonly loading = signal(false);
+  readonly error = signal<string | null>(null);
+  readonly total = computed(() => this.cartService.total());
+
+  constructor(private itemService: ItemService,
+    readonly cartService: CartService,
+    private offerService: OfferService,
+     private router: Router) {
+
+    this.loadData();
+
     effect(() => {
-      console.log('>>>', this.itemService.items());
-    });
-  }  
+      const items = this.itemService.items();
+      const offers = this.offerService.offers();
+      const cartItems = this.cartService.items();
 
-  ngOnInit(): void {
-    this.itemService.getAllItems();
+      this.items.set(
+        items.map(item => {
+          const cartItem = cartItems.find(ci => ci.item.id === item.id);
+          return {
+            ...item,
+            quantity: cartItem?.quantity ?? 0,
+            specialOffer: this.getSpecialOffer(item, offers)
+          };
+        })
+      );
+    });
+  }
+
+  goToCheckout() {
+    this.router.navigate(['/checkout']);
+  }
+
+  private async loadData(): Promise<void> {
+    this.loading.set(true);
+    try {
+      await Promise.all([
+        this.itemService.getAllItems(),
+        this.offerService.getAllOffers(),
+        this.cartService.getCartContents(),
+        this.cartService.getCartTotal()
+      ]);
+    } catch (err) {
+      this.error.set('Erro ao carregar os dados.');
+    } finally {
+      this.loading.set(false);
+    }
+  }
+
+  private getSpecialOffer(item: Item, offers: Offer[]): string | undefined {
+    const offer = offers.find(o => o.item.id === item.id);
+    return offer ? `${offer.quantity} for €${(offer.totalPrice / 100).toFixed(2)}` : undefined;
+  }
+
+  increaseQuantity(item: ItemWithQuantity): void {
+    this.cartService.scanItem(item.id);
+  }
+
+  decreaseQuantity(item: ItemWithQuantity): void {
+    this.cartService.decreaseItem(item.id);
   }
 
 }
+
 
 
